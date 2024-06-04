9bc62dfee0cc793e1864cbb06d6c131cf08d5ea4
jiaming <jamie-w@nus.edu.sg>
2024-06-04 09:53:29 +0800

    gitignore

--- evaluate_local.py
+++ evaluate_local.py
@@ -2,8 +2,10 @@ import sys
 sys.path.insert(0, "")
 import numpy as np
 import habitat
+import hydra
 import os
-
+from omegaconf import OmegaConf
+os.environ["CHALLENGE_CONFIG_FILE"] = "./configs/mopa/orasem.yaml" #"./configs/multinav.yaml"
 
 # add mc_agent path: ~/multion-challenge/mc2-agent
 sys.path.insert(0, "/home/users/jiaming/multion/mc2-agent")
@@ -11,30 +13,30 @@ sys.path.insert(0, "/home/users/jiaming/multion/mc2-agent")
 from mc2.agent.mc2_agent import MC2Agent
 from mc2.tasks.multion.interactive_evaluator import InteractiveEvaluator
 
-os.environ["CHALLENGE_CONFIG_FILE"] = "./configs/multinav.yaml"
 os.environ["AGENT_EVALUATION_TYPE"] = "local"
 
-class RandomWalker(habitat.Agent):
-    def __init__(self, task_config: habitat.Config):
-        self._POSSIBLE_ACTIONS = task_config.TASK.POSSIBLE_ACTIONS
-
-    def reset(self):
-        pass
-
-    def act(self, observations):
-        print(f'Observations: {observations}')
-        # return {"action": np.random.choice(self._POSSIBLE_ACTIONS)}
-        return {"action": "FOUND"}
+from habitat.core.env import Env, LangMONEnv
+from habitat.config.default import get_config
 
 def main():
-    
-    config = habitat.get_config("./configs/multinav.yaml")
-    agent = MC2Agent(task_config=config)
-    
+    config_paths = os.environ["CHALLENGE_CONFIG_FILE"]
+    config_env = get_config(config_paths)
+    env = LangMONEnv(config=config_env)
+            
+            
+    agent = MC2Agent(task_config=config_env)
+
     evaluator = InteractiveEvaluator(config_paths="./configs/multinav.yaml")
 
     evaluator.play()
 
-    
+
+
+from multion.config.structured_configs import register_hydra_plugin
+from configs.structured_configs import (
+    HabitatBaselinesConfigPlugin,
+)
+
 if __name__ == "__main__":
+    register_hydra_plugin(HabitatBaselinesConfigPlugin)
     main()
